714e9934e71941a5a0717fe62bd6bdcd0e57250d
gab-borges <soliid002@gmail.com>
2025-12-16 17:26:25 -0300

    utf.c 2025 A

---
+++ problems/groups/cdp/658993A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+#define debug(x) cout << #x << " = " << x << '\n';
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << ' '; cout << '\n';
+#define vvdebug(a) cout << #a <<" ="<<endl; for(auto &row:a){for(auto &x:row) cout<<x<<' '; cout << endl;}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t, o; cin >> t >> o;
+
+    if (20 <= t && t <= 30 && o >= 50) {
+        cout << "HABITAVEL\n";
+    }
+    else if (0 <= t && t <= 50 && o >= 30) {
+        cout << "ADAPTAVEL\n";
+    }
+    else {
+        cout << "HOSTIL\n";
+    }
+
+    return 0;
+}
