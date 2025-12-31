2a3a49f7026fd90d75e54d79ff95207300599a34
gab-borges <soliid002@gmail.com>
2025-12-30 23:24:09 -0300

    2179A

---
+++ problems/2179A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+#define debug(x) cout << #x << " = " << x << '\n';
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << ' '; cout << '\n';
+#define vvdebug(a) cout << #a <<" ="<<endl; for(auto &row:a){for(auto &x:row) cout<<x<<' '; cout << endl;}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t; cin >> t;
+
+    while(t--) {
+        int k, x; cin >> k >> x;
+        cout << k*x + 1 << '\n';
+    }
+
+    return 0;
+}
