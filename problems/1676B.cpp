64bce7fb4329c412c1e6715cd58628e164258200
gab-borges <soliid002@gmail.com>
2026-02-06 20:16:24 -0300

    1676B

---
+++ problems/1676B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+#define debug(x) cout << #x << " = " << x << '\n';
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << ' '; cout << '\n';
+#define vvdebug(a) cout << #a <<" ="<<endl; for(auto &row:a){for(auto &x:row) cout<<x<<' '; cout << endl;}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n; cin >> n;
+
+        int menor = INT_MAX;
+        vi A(n);
+        for (int i = 0; i < n; i++) { cin >> A[i]; menor = min(menor, A[i]); }
+
+        ll ans = 0;
+        for (int i = 0; i < n; i++) { ans += A[i] - menor; }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
