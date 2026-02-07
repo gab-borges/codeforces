0912a88d6585cc9fa3c20704741fe550c8976d7a
gab-borges <soliid002@gmail.com>
2026-02-06 21:01:33 -0300

    2093A

---
+++ problems/2093A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+#define debug(x) cout << #x << " = " << x << '\n';
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << ' '; cout << '\n';
+#define vvdebug(a) cout << #a <<" ="<<endl; for(auto &row:a){for(auto &x:row) cout<<x<<' '; cout << endl;}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t; cin >> t;
+
+    while (t--) {
+        int k; cin >> k;
+
+        if (k & 1) cout << "YES\n";
+        else cout << "NO\n";
+    }
+
+    return 0;
+}
